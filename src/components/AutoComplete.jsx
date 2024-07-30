d156d415f8148003d7bccc9d667ba74870103a12
hayitmurod707 <hayitmurod707@gmail.com>
2024-07-30 19:46:20 +0500

    updated

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -1,16 +1,35 @@
-import { any, array, bool, func, string } from 'prop-types';
-import { memo } from 'react';
+import { any, bool, func, string } from 'prop-types';
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import ReactSelect, { components } from 'react-select';
+import { telemarketingAPI } from 'services/api';
 import styled, { keyframes } from 'styled-components';
+import { useDebouncedCallback } from 'use-debounce';
 const animation1 = keyframes`
    0% {
       clip-path: polygon(0% 0%, 0% 0%, 0% 0%, 50% 50%, 0% 0%, 0% 0%, 0% 0%);
    }
    50% {
-      clip-path: polygon( 0% 0%, 0% 100%, 0% 100%, 50% 50%, 100% 0%, 100% 0%, 0% 0% );
+      clip-path: polygon(
+         0% 0%,
+         0% 100%,
+         0% 100%,
+         50% 50%,
+         100% 0%,
+         100% 0%,
+         0% 0%
+      );
    }
    100% {
-      clip-path: polygon( 0% 0%, 0% 100%, 100% 100%, 50% 50%, 100% 100%, 100% 0%, 0% 0% );
+      clip-path: polygon(
+         0% 0%,
+         0% 100%,
+         100% 100%,
+         50% 50%,
+         100% 100%,
+         100% 0%,
+         0% 0%
+      );
    }
 `;
 const animation2 = keyframes`
@@ -44,10 +63,10 @@ const StyledControl = styled.div`
       padding: 0 0 0 16px;
       width: calc(100% - 66px);
       & input {
-         display: flex;
-         max-width: 100% !important;
-         min-width: 100% !important;
          opacity: 1 !important;
+         min-width: 100% !important;
+         max-width: 100% !important;
+         display: flex;
       }
    }
    & .indicator-content {
@@ -57,14 +76,12 @@ const StyledControl = styled.div`
       justify-content: flex-end;
       padding: 0 12px 0 8px;
       width: 66px;
-      & .clear-loading {
+      & .loading-content {
          align-items: center;
          display: flex;
          height: 23px;
          justify-content: center;
-         margin: 0 6px 0 0;
-         min-width: 20px;
-         width: 20px;
+         width: 23px;
          & .loading {
             height: 23px;
             width: 23px;
@@ -78,6 +95,13 @@ const StyledControl = styled.div`
                width: 23px;
             }
          }
+      }
+      & .clear-button {
+         align-items: center;
+         display: flex;
+         height: 23px;
+         justify-content: center;
+         width: 23px;
          & button {
             align-items: center;
             background-color: transparent;
@@ -88,7 +112,6 @@ const StyledControl = styled.div`
             display: flex;
             height: 23px;
             justify-content: center;
-            margin: 0;
             outline: none;
             padding: 0;
             width: 23px;
@@ -118,18 +141,20 @@ const Control = props => {
          <StyledControl>
             <div className='inner-content'>{children}</div>
             <div className='indicator-content'>
-               <div className='clear-loading'>
-                  {isLoading && !isDisabled ? (
+               {isLoading && !isDisabled ? (
+                  <div className='loading-content'>
                      <div className='loading'>
                         <div></div>
                      </div>
-                  ) : inputValue && !isDisabled ? (
+                  </div>
+               ) : inputValue && !isDisabled ? (
+                  <div className='clear-button'>
                      <button
-                        type='button'
-                        data-focused={isFocused}
                         onClick={() => {
                            onChange('', { action: 'set-value' });
                         }}
+                        type='button'
+                        data-focused={isFocused}
                      >
                         <svg
                            fill='none'
@@ -153,8 +178,8 @@ const Control = props => {
                            />
                         </svg>
                      </button>
-                  ) : null}
-               </div>
+                  </div>
+               ) : null}
                <div className='dropdown-button'>
                   <svg height='18' width='18' viewBox='0 0 20 20'>
                      <path
@@ -170,7 +195,7 @@ const Control = props => {
 };
 const Menu = ({ isLoading, children, ...props }) =>
    !isLoading && <components.Menu {...props}>{children}</components.Menu>;
-const defaultOptions = {
+const defaultProps = {
    components: {
       Control,
       IndicatorsContainer: () => null,
@@ -198,22 +223,22 @@ const styles = {
    }),
    placeholder: styles => ({
       ...styles,
-      alignItems: 'center',
+      fontSize: 16,
+      fontWeight: 500,
+      margin: 0,
+      position: 'absolute',
+      left: 0,
+      top: 0,
+      padding: '0 8px 0 0',
       color: '#717171',
       display: 'flex',
-      fontSize: 17,
-      fontWeight: 500,
+      alignItems: 'center',
       height: '100%',
-      left: 0,
-      margin: 0,
-      padding: '0 8px 0 0',
-      position: 'absolute',
-      top: 0,
    }),
    input: (styles, { isDisabled }) => ({
       ...styles,
       color: isDisabled ? '#717171' : '#212121',
-      fontSize: 17,
+      fontSize: 16,
       fontWeight: 500,
       height: '100%',
       left: 0,
@@ -250,130 +275,140 @@ const styles = {
          borderRadius: 3,
       },
    }),
-   option: (styles, { isSelected, isDisabled, isFocused }) => ({
-      ...styles,
-      backgroundColor: isDisabled
-         ? '#f7f8fc'
-         : isSelected
-         ? '#3a79f3'
-         : isFocused
-         ? 'rgba(82, 85, 241, 0.1)'
-         : '#ffffff',
-      borderRadius: 10,
-      color: isDisabled
-         ? '#696f85'
-         : isSelected
-         ? '#ffffff'
-         : isFocused
-         ? '#252a3b'
-         : '#252a3b',
-      cursor: isDisabled ? 'not-allowed' : 'pointer',
-      fontSize: 17,
-      fontWeight: 500,
-      height: 44,
-      overflow: 'hidden',
-      padding: '11px 12px',
-      textOverflow: 'ellipsis',
-      transition: 200,
-      transitionTimingFunction: 'cubic-bezier(0, 0, 1, 1)',
-      whiteSpace: 'nowrap',
-      width: '100%',
-      ':hover': {
-         backgroundColor: isDisabled
-            ? '#f7f8fc'
-            : isSelected
-            ? '#3a79f3'
-            : 'rgba(82, 85, 241, 0.1)',
-      },
-   }),
    noOptionsMessage: styles => ({
       ...styles,
-      color: 'rgb(37, 42, 59)',
-      cursor: 'not-allowed',
-      fontSize: 17,
+      color: '#000000',
+      cursor: 'default',
+      fontSize: 16,
       fontWeight: 500,
-      height: 44,
+      height: 40,
       padding: '10px 12px',
       textAlign: 'left',
       width: '100%',
    }),
 };
-const AutoComplete = memo(
-   ({
-      isDisabled = false,
-      isError = false,
-      isLoading = false,
-      noOptionsMessage = null,
-      onChange,
-      options = [],
-      placeholder = '',
-      setLoading,
-      value = null,
-      ...props
-   }) => {
-      const NoOptionsMessage =
-         typeof noOptionsMessage === 'function'
-            ? noOptionsMessage
-            : () => "Ma'lumot topilmadi";
-      const onInputChange = (option, { action }) => {
-         if (action !== 'input-blur' && action !== 'menu-close') {
-            const search =
-               typeof option?.label === 'string' ? option.label : value;
-            onChange(search);
-         }
-      };
-      return (
-         <ReactSelect
-            {...defaultOptions}
-            {...props}
-            inputValue={typeof value === 'string' ? value : value?.label}
-            isDisabled={isDisabled}
-            isLoading={isLoading}
-            noOptionsMessage={NoOptionsMessage}
-            onChange={onChange}
-            onInputChange={onInputChange}
-            options={options}
-            placeholder={placeholder}
-            value={null}
-            styles={{
+const AutoComplete = ({
+   getResponse,
+   getUrl,
+   isDisabled = false,
+   isError = false,
+   noOptionsMessage = null,
+   onChange,
+   onFocus,
+   placeholder = '',
+   value = null,
+   ...props
+}) => {
+   const { t } = useTranslation('components');
+   const [loading, setLoading] = useState(false);
+   const [options, setOptions] = useState([]);
+   const debouncedSearch = useDebouncedCallback(async search => {
+      await setLoading(true);
+      const url = getUrl(search);
+      telemarketingAPI({ url })
+         .then(response => setOptions(getResponse(response)))
+         .catch(() => setOptions([]))
+         .finally(() => setLoading(false));
+   }, 300);
+   const onInputChange = (option, { action }) => {
+      if (action !== 'input-blur' && action !== 'menu-close') {
+         onChange(option);
+      }
+   };
+   const NoOptionsMessage =
+      typeof noOptionsMessage === 'function'
+         ? noOptionsMessage
+         : () => t('not-found');
+   useEffect(() => {
+      const search = typeof value === 'string' ? value : value?.label;
+      debouncedSearch(search);
+   }, [value, debouncedSearch]);
+   return (
+      <ReactSelect
+         {...defaultProps}
+         {...props}
+         inputValue={typeof value === 'string' ? value : value?.label}
+         isDisabled={isDisabled}
+         isLoading={loading}
+         noOptionsMessage={NoOptionsMessage}
+         onChange={onChange}
+         onFocus={onFocus}
+         onInputChange={onInputChange}
+         options={options}
+         placeholder={placeholder}
+         value={null}
+         styles={{
+            ...styles,
+            option: (styles, { isDisabled, isFocused, data }) => ({
                ...styles,
-               control: (styles, { isFocused }) => ({
-                  ...styles,
-                  backgroundColor: '#ffffff',
+               backgroundColor: isDisabled
+                  ? '#f7f8fc'
+                  : data?.value === value?.value
+                  ? '#3a79f3'
+                  : isFocused
+                  ? 'rgba(82, 85, 241, 0.1)'
+                  : '#ffffff',
+               borderRadius: 10,
+               color: isDisabled
+                  ? '#696f85'
+                  : data?.value === value?.value
+                  ? '#ffffff'
+                  : isFocused
+                  ? '#252a3b'
+                  : '#252a3b',
+               cursor: isDisabled ? 'not-allowed' : 'pointer',
+               fontSize: 16,
+               fontWeight: 500,
+               height: 44,
+               overflow: 'hidden',
+               padding: '11px 12px',
+               textOverflow: 'ellipsis',
+               transition: 200,
+               whiteSpace: 'nowrap',
+               width: '100%',
+               ':hover': {
+                  backgroundColor: isDisabled
+                     ? '#f7f8fc'
+                     : data?.value === value?.value
+                     ? '#3a79f3'
+                     : 'rgba(82, 85, 241, 0.1)',
+               },
+            }),
+            control: (styles, { isFocused, isDisabled }) => ({
+               ...styles,
+               backgroundColor: isDisabled ? '#f4f4f4' : '#ffffff',
+               border: `1.5px solid ${
+                  isError ? '#e41d32' : isFocused ? '#397af5' : '#e1e1e1'
+               }`,
+               borderRadius: 10,
+               boxShadow: 'none',
+               color: 'rgb(37, 42, 59)',
+               cursor: isDisabled ? 'default' : 'text',
+               height: 46,
+               minHeight: 46,
+               minWidth: 100,
+               outline: 'none',
+               padding: 0,
+               width: '100%',
+               ':hover': {
                   border: `1.5px solid ${
                      isError ? '#e41d32' : isFocused ? '#397af5' : '#e1e1e1'
                   }`,
-                  borderRadius: 10,
-                  boxShadow: 'none',
-                  color: 'rgb(37, 42, 59)',
-                  cursor: 'text',
-                  height: 46,
-                  minHeight: 46,
-                  minWidth: 100,
-                  outline: 'none',
-                  padding: 0,
-                  width: '100%',
-                  ':hover': {
-                     border: `1.5px solid ${
-                        isError ? '#e41d32' : isFocused ? '#397af5' : '#e1e1e1'
-                     }`,
-                  },
-               }),
-            }}
-         />
-      );
-   }
-);
+               },
+            }),
+         }}
+      />
+   );
+};
 AutoComplete.propTypes = {
+   getResponse: func,
+   getUrl: func,
    isDisabled: bool,
    isError: bool,
-   isLoading: bool,
    noOptionsMessage: any,
    onChange: func,
    onFocus: func,
-   options: array,
    placeholder: string,
-   setLoading: func,
    value: any,
 };
 export default AutoComplete;
---
+++ src/components/NumberInput.jsx
@@ -0,0 +1,68 @@
+import CleaveInput from 'cleave.js/react';
+import { bool, func, string } from 'prop-types';
+import { forwardRef, memo } from 'react';
+import styled from 'styled-components';
+const StyledInput = styled(CleaveInput)`
+   background-color: transparent;
+   border-radius: 10px;
+   border: 1.5px solid #e1e1e1;
+   font-size: 16px;
+   font-weight: 500;
+   height: 46px;
+   outline: none;
+   padding-left: 15px;
+   width: 100%;
+   &[data-error='true'] {
+      border-color: #e41d32;
+   }
+   &:disabled {
+      background-color: #f4f4f4;
+      color: #717171;
+      cursor: default;
+   }
+   &:focus {
+      border: 1.5px solid #3a79f3;
+   }
+`;
+const NumberInput = memo(
+   forwardRef(
+      (
+         {
+            isDisabled = false,
+            isError = false,
+            onChange,
+            onFocus,
+            placeholder = '',
+            value = '',
+         },
+         ref
+      ) => (
+         <StyledInput
+            data-error={isError}
+            disabled={isDisabled}
+            inputMode='numeric'
+            onChange={e => onChange(e.target.value.replace(/ /g, ''))}
+            onFocus={onFocus}
+            placeholder={placeholder}
+            ref={ref}
+            type='text'
+            value={value}
+            options={{
+               delimiter: ' ',
+               numeral: true,
+               numeralThousandsGroupStyle: 'thousand',
+            }}
+         />
+      ),
+      {}
+   )
+);
+NumberInput.propTypes = {
+   isDisabled: bool,
+   isError: bool,
+   onChange: func,
+   onFocus: func,
+   placeholder: string,
+   value: string,
+};
+export default NumberInput;
